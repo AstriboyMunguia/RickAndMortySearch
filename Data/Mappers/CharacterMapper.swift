5bb17839cc8c073dd7bc2f07cc0f700ab3c0291f
Eduardo Geovanni Perez Munguia <lalomunguia22@gmail.com>
2026-05-13 22:47:41 -0600

    Add character mapper

--- Data/Mappers/CharacterMapper.swift
+++ Data/Mappers/CharacterMapper.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum CharacterMapper {
+    static func map(_ dto: CharacterDTO) -> Character {
+        Character(
+            id: dto.id,
+            name: dto.name,
+            status: dto.status,
+            species: dto.species,
+            gender: dto.gender,
+            locationName: dto.location.name,
+            image: dto.image,
+            episodeURLs: dto.episode,
+            latitude: Double.random(in: 19.0...19.8),
+            longitude: Double.random(in: -99.5 ... -98.8)
+        )
+    }
+}
+
